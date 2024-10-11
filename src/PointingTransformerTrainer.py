8827b207c5ae55d929507063cae5e35fa9f0bc30
DavidTung-TRI <david.tung@tri.global>
2024-10-10 17:05:17 -0700

    import torch

--- src/PointingTransformerTrainer.py
+++ src/PointingTransformerTrainer.py
@@ -1,3 +1,7 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 from praxis.PointingTransformer import YOLOBackbone, PointingDeviceClassification
 
 # Define number of classes for your dataset
