275edc6f27ada330ed42cc4782cb7b2021a4adcc
DavidTung-TRI <david.tung@tri.global>
2024-10-25 12:14:05 -0700

    add scripts livingroom

--- data/setup.sh
+++ data/setup.sh
@@ -3,3 +3,4 @@ sudo mount -t nfs4 -o nfsvers=4.1 fs-02402528f441d97bf.efs.us-east-1.amazonaws.c
 
 cd /mnt/efs/DPDataset_public
 sudo mount -o loop frames_squashed/2023-01-17-openoffice frames/2023-01-17-openoffice
+sudo mount -o loop frames_squashed/2023-01-17-livingroom frames/2023-01-17-livingroom
